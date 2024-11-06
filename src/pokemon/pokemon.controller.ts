5ba13cc60c8c2f787c1342e3967bc64a24c5a255
Duvan <duvanriano2626@gmail.com>
2024-11-06 14:51:15 -0500

    Se configura el findOne para que pueda hacer la respectiva busqueda por nombre de pokemon id del pokemon o numero de poquemon muy importante con sus respectivas evaluaciones

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -17,9 +17,9 @@ export class PokemonController {
     return this.pokemonService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.pokemonService.findOne(+id);
+  @Get(':busqueda')
+  findOne(@Param('busqueda') busqueda: string) {
+    return this.pokemonService.findOne(busqueda);
   }
 
   @Patch(':id')
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PokemonService {
@@ -37,8 +38,31 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(busqueda: string) {
+    let pokemon:Pokemon;
+
+    //Busca por numero en la base de datos (no)
+    if (!isNaN(+busqueda)) {
+      pokemon = await this.pokemonModel.findOne({no:busqueda})
+    }
+
+    //Busca por MongoId
+    if (!pokemon && isValidObjectId(busqueda)) {
+      pokemon = await this.pokemonModel.findById(busqueda);
+    }
+
+    //Busca por Nombre
+
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({name:busqueda.toLowerCase().trim()})
+    }
+
+    //Si no encuentra nada
+    if (!pokemon) {
+      throw new NotFoundException(`Pokemon with id, name or no "${busqueda}" not found`);
+    }
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
