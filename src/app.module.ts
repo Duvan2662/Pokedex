df5df766633cc65466d741f7d33f97b86bb97eee
Duvan <duvanriano2626@gmail.com>
2024-11-08 10:56:37 -0500

    Se crea un archivo para que configure las variables de entorno si no estan configuradas

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { EnvConfiguration } from './common/config/env.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(
+      {
+        load:[EnvConfiguration]
+      }
+    ), //Necesario el paquete leer la guia de fernando en Nest variables de entorno ultima hoja
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule,
