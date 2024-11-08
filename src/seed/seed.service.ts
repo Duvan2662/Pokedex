216977c2471d842ebbeda18ca3d675564707bae3
Duvan <duvanriano2626@gmail.com>
2024-11-07 21:06:40 -0500

    Se crea un adaptador por si en algun momento se requiere cambiar del paquete de axios que hace las peticiones a la API de pokemon por otro paquete como fetch revisar las clases de nest donde fernando esta dando la introduccion a typesScript en nest

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,6 +3,8 @@ import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 import { PokemonService } from '../pokemon/pokemon.service';
+import { HttpAdapter } from 'src/common/interfaces/http-adapter.interface';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapte';
 
 
 @Injectable()
@@ -11,12 +13,11 @@ export class SeedService {
 
   constructor(
     private readonly  pokemonServices:PokemonService,
+    private readonly http:AxiosAdapter,
   ){
 
   }
 
-  private readonly axios:AxiosInstance = axios;
-  
   public async executeSeed() {
 
     //Sirve para eliminar los datos que ya existen en la base de datos 
@@ -29,7 +30,7 @@ export class SeedService {
 
     //Insertar datos desde la Api de pokeApi que esta en la web
     const pokemonInsert: {name:string, no: number}[] = [];
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data  = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
     data.results.forEach(({ name, url }) => {
       
       const segments = url.split('/');
